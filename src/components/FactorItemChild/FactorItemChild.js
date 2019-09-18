b205ad8f6369d8ae84de733170ed8ef85388c7d3
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-09-18 15:42:18 -0500

    reducer map attempt

--- src/components/FactorItemChild/FactorItemChild.js
+++ src/components/FactorItemChild/FactorItemChild.js
@@ -5,11 +5,28 @@ import mapStateToProps from '../../redux/mapRedux/mapStateToProps';
 
 const FactorItemChild = ({factorsWeights, deleteFactor}) => {
 
-  const factorList = factorsWeights.length ? (
-    factorsWeights.map(factorsWeights => {
+  // const factorList = factorsWeights.length ? (
+  //   factorsWeights.map(factorsWeights => {
+  //     return (
+  //       <div key={factorsWeights.id}>
+  //         <button onClick={() => {deleteFactor(factorsWeights.id)}}>`{factorsWeights.factors} {factorsWeights.weight}`</button>
+  //       </div>
+  //     )
+  //   })
+  // ) : (
+  //   <p>You need at least one factors to use this app</p>
+  // )
+  // return (
+  //   <div>
+  //     {factorList}
+  //   </div>
+  // )
+
+  const factorList = this.props.redux.factorWeightReducer.length ? (
+    this.props.redux.factorWeightReducer.map(( dummydata, index ) => {
       return (
-        <div key={factorsWeights.id}>
-          <button onClick={() => {deleteFactor(factorsWeights.id)}}>`{factorsWeights.factors} {factorsWeights.weight}`</button>
+        <div key={index}>
+          <button onClick={() => {deleteFactor(index)}}>`{dummydata.factors} {dummydata.weight}`</button>
         </div>
       )
     })
