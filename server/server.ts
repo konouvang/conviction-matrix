08a03f89bb5cf841b3799ec8c7de3c8b55d9e9f3
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-09-26 12:33:59 -0500

    reconfiguring server to fit mongodb - cors was missing so npm start would not work, cors installed

--- server/server.ts
+++ server/server.ts
@@ -1,85 +1,14 @@
-// import express from 'express';
-import bodyParser from 'body-parser';
-import sessionMiddleware from './modules/session-middleware';
-import passport from './strategies/user.strategy';
-// import userRouter from './routes/user.router';
-// import adminRouter from './routes/admin.router';
-// import reviewRouter from './routes/review.router';
-// import appointmentRouter from './routes/appointment.router';
-// import measurementsRouter from './routes/measurements.router';
-// import resourcesRouter from './routes/resources.router';
-// import needRouter from './routes/need.router';
-// import infoRouter from './routes/info.router';
-import decisionItemRouter from './routes/decisionItem.router';
-import factorWeightRouter from './routes/factorWeight.router';
-import scoreRouter from './routes/score.router';
+const express = require('express');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app: any = express();
-
-//************Konou Note: From MERN Tutorial - needed to connect to the database
-
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-// require('dotenv').config();
-
-// const app = express();
-// const port = process.env.PORT || 5000;
+const PORT: number | string = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// app.listen(port, () => {
-//     console.log(`Server is running on port: ${port}`);
-// });
-
-//***********End of Konou Note
-
-// Body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Passport Session Configuration //
-app.use(sessionMiddleware);
-
-// start up passport sessions
-app.use(passport.initialize());
-app.use(passport.session());
-
-/* Routes */
-// app.use('/api/user', userRouter);
-// app.use('/api/admin', adminRouter);
-// app.use('/api/review', reviewRouter);
-// app.use('/api/appointment', appointmentRouter);
-// app.use('/api/measurements', measurementsRouter);
-// app.use('/api/info', infoRouter);
-// app.use('/api/resources', resourcesRouter);
-// app.use('/api/need', needRouter);
-app.use('/api/decision', decisionItemRouter);
-app.use('/api/factor', factorWeightRouter);
-app.use('/api/score', scoreRouter);
-
-
-// Serve static files
-app.use(express.static('build'));
-
-// App Set //
-const PORT: number | string = process.env.PORT || 5000;
-
-/** Listen * */
 app.listen(PORT, (): void => {
-  console.log(`So awesome. Much wow. Listening on port: ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
-
-// Twilio
-// app.get('/test/scott', (req: any, res: any) => {
-//   textGentleman('+SOME_NUMBER_HERE', 'SOME_MESSAGE');
-//   res.send(200);
-// })
-
-
-
-export default app;
